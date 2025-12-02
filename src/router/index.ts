c5753ab8ad5f0e6e1c1e0684ad9aa07b69c085bf
Ritika <ritika@secureitlabservices.com>
2025-12-02 13:55:55 +0530

    Main Signin authentication api

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,11 +1,18 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import MainSignin from '@/components/MainSignin.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
+      name: 'signin',
+      component: MainSignin,
+    },
+
+    {
+      path: '/home',
       name: 'home',
       component: HomeView,
     },
